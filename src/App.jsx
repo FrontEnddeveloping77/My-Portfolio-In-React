6887eb0c036eccd23c3ad868a567b4b811815b44
abdulazizavazov2508@gmail.com <abdulazizavazov2508@gmail.com>
2025-12-27 19:09:16 +0500

    my-portfolio-in-react

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Projects from './components/Projects/Projects';
 import Contact from './components/Contact/Contact';
 import Footer from './components/Footer/Footer';
 import './App.css';
+// import ScrollToTop from './components/ToTopBtn/totopbtn';
 
 function App() {
     const [theme, setTheme] = useState('light');
